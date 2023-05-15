32935ab780309e54668467c7343471158a43c7de
Maria Vitória Galvão <mariaviih772@gmail.com>
2023-05-15 08:40:11 -0300

    Inserção de Anotação de dados nas classes

--- GalloFlix/Models/Genre.cs
+++ GalloFlix/Models/Genre.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GalloFlix.Models;
 
     public class Genre
     {
+        [Key] // é uma anotação para a chave primária
         public byte id { get; set; }
+        
+        [Required] // Requerido - Not null, no banco; Torna o nome obrigatório
+        [StringLength(30)] // Tamanho máximo da propriedade
         public string Name { get; set; }
     }
+
